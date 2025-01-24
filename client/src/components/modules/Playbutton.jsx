3889c919fb8c62e06b524af77903922c4b0f17e5
Lillian Bluestein <lillyblue04@icloud.com>
2025-01-24 00:25:38 -0500

    added dropdown when clicking profile photo to have navigation to profile page and log out; made waveform animation fill the whole canvas

--- client/src/components/modules/Playbutton.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Playbutton.css"
-
-const PlayButton = ({ onClick }) => {
-  return <button className="play-button" onClick={onClick}>Play Note</button>;
-};
-
-export default PlayButton;
